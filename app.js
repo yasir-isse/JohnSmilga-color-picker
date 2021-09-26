fa6dc05c48852680148173e905a5f3b834c80ad6
Yasir <code.yasir@gmail.com>
2021-09-26 12:50:28 +0300

    add logic

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+const randomGenerator = () => Math.floor(Math.random() * colors.length);
+
+btn.addEventListener("click", () => {
+  const randomNumber = randomGenerator();
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
