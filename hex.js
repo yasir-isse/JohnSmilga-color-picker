cbe24be507f498ad0917170a16ec324ef02c0c0c
Yasir <code.yasir@gmail.com>
2021-09-26 13:33:53 +0300

    add hex logic

--- hex.js
+++ hex.js
@@ -1,1 +1,18 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+const randomNumber = () => Math.floor(Math.random() * hex.length);
+
+const hexGenerator = () => {
+  let randomSelect = "#";
+  for (let i = 0; i < 6; i++) {
+    randomSelect += hex[randomNumber()];
+  }
+  return randomSelect;
+};
+
+btn.addEventListener("click", () => {
+  document.body.style.backgroundColor = hexGenerator();
+  color.textContent = hexGenerator();
+});
